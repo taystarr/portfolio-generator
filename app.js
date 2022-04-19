621b32bc3e2e5100995673efd0e3195ddb3ee1b4
Tay Starr <tstarrroberts@gmail.com>
2022-04-18 21:36:08 -0400

    add template for info input

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -119,7 +122,7 @@ const promptProject = portfolioData => {
         {
             type: 'confirm',
             name: 'confirmAddProject',
-            message: 'Would you liketo enter another project?',
+            message: 'Would you like to enter another project?',
             default: false
         }
     ])
@@ -136,17 +139,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
 
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
